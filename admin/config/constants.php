d39d47d8ce5f1d9e55ea040b1b8132910c222708
Omar-AB42 <79457450+Omar-AB42@users.noreply.github.com>
2026-07-19 11:34:19 +0300

    Add files via upload

---
+++ admin/config/constants.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+define('ROOT_URL', 'http://localhost/kaestates/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'Omar');
+define('DB_PASS', 's9lthese9uel');
+define('DB_NAME', 'kaestates');
